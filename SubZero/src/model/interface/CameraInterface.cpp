cad09dbbd1d12163c4c57680fbc3374acf9d5191
ehsueh <ehsueh1993@gmail.com>
2015-06-13 15:20:13 -0400

    Added codes for CameraInterface. Refactored interface codes. To be tested.

--- SubZero/src/model/interface/CameraInterface.cpp
+++ SubZero/src/model/interface/CameraInterface.cpp
@@ -7,58 +7,72 @@
 
 #include "CameraInterface.h"
 
-	/* ==========================================================================
-	 * 				INTERACTING WITH DATA COMING IN (FROM Camera)
-	 * ==========================================================================
-	 * This interface class will be automatically polling and managing
-	 * the polling process privately within the interface at pollFrequency
-	 * using the functions below.
-	 */
+/* ==========================================================================
+ * 				INTERACTING WITH DATA COMING IN (FROM Camera)
+ * ==========================================================================
+ * This interface class will be automatically polling and managing
+ * the polling process privately within the interface at pollFrequency
+ * using the functions below.
+ */
 
-	/**
-	 * Poll raw data from the camera.
-	 * @return	data polled
-	 */
+/**
+ * Poll raw data from the camera.
+ * @return	data polled
+ */
 
-	void CameraInterface::poll() {
+void CameraInterface::poll() {
+
+	cv::Mat raw;
+	camStream.read(raw);
+	Data* decoded = this->decode(&raw);
+	this->storeToBuffer(decoded);
+
+};
+
+/**
+ * Decode the data.
+ * @param	data	data to be decoded
+ * @return	decoded data in a ImgData format
+ */
+ImgData* CameraInterface::decode(cv::Mat* data) {
+
+	ImgData decoded = new ImgData("raw", data);
+	return decoded;
+
+};
+
+/* ==========================================================================
+ * 							CONSTRUCTOR AND DESTRUCTOR
+ * ==========================================================================
+ */
 
 
-	};
+CameraInterface::CameraInterface(int bufferSize, int pollFrequency) {
 
-	/**
-	 * Decode the data.
-	 * @param	data	data to be decoded
-	 * @return	decoded data in a ImgData format
-   	 */
-	ImgData* CameraInterface::decode(cv::Mat* data) {
+	this->bufferSize = bufferSize;
+	this->pollFrequency = pollFrequency;
+	this->camStream(0);
 
-	};
+	// thread for reading and polling camera input
+   readThreads.push_back(std::thread(&FPGAInterface::in, this));
 
+}
 
-	/* ==========================================================================
-	 * 							MANAGING DATA BUFFER
-	 * ==========================================================================
-	 * The data buffer will be managed automatically and privately by this class.
-	 * These functions are defined and implemented in the root parent class i.e.
-	 * in HwInterface.
-	 */
+CameraInterface::~CameraInterface() {
 
-	void CameraInterface::deleteFromBuffer(int startIdx, int endIdx);
+	// join readThread with main
+	for(auto& t: readThreads) {t.join();}
 
-	void CameraInterface::storeToBuffer(Data* data);
+	// clears the queue
+	while ( ! decodedBuffer.empty()) {
+		decodedBuffer.pop();
+	}
+	// not sure if the above also frees up memory used up by the "queue container"
+	// whatever the container may be... delete it by following the pointer
+	delete &(this->decodedBuffer);
+	delete &(this->bufferSize);
+	delete &(this->pollFrequency);
+	delete position;
+	delete camStream;
 
-public:
-
-	/* ==========================================================================
-	 * 							CONSTRUCTOR AND DESTRUCTOR
-	 * ==========================================================================
-	 */
-
-	CameraInterface::CameraInterface(int bufferSize, int pollFrequency, CameraPosition position) {
-		camStream(this.position);
-	};
-
-	CameraInterface::~CameraInterface() {
-		delete camStream;
-	};
-
+}
